d714c012f2168d5c1bfb748d710fb07c651d0eb1
Mihail <jktrut@gmail.com>
2023-10-26 16:54:23 +0300

    First commit

---
+++ panda.py
@@ -0,0 +1,144 @@
+import turtle
+turtle.bgcolor("pink")
+
+painter = turtle.Turtle()
+
+painter.pensize(3)
+painter.speed(1)
+
+#Draw body
+
+painter.begin_fill()
+painter.color('black', 'black')
+painter.pendown()
+painter.circle(120)
+painter.end_fill()
+
+
+#Draw face
+
+painter.begin_fill()
+painter.color('white', 'white')
+painter.pendown()
+painter.circle(100)
+painter.end_fill()
+
+
+#Draw right ear
+
+painter.color('gray', 'gray')
+painter.penup()
+painter.setx(50)
+painter.sety(185)
+painter.pendown()
+
+painter.begin_fill()
+painter.right(90)
+painter.circle(30, -260)
+painter.end_fill()
+
+
+#Draw left ear
+
+painter.color('gray', 'gray')
+painter.penup()
+painter.setx(-50)
+painter.sety(185)
+painter.pendown()
+
+painter.left(170)
+painter.begin_fill()
+painter.right(90)
+painter.circle(30, 260)
+painter.end_fill()
+
+
+#Draw left eye
+
+painter.penup()
+painter.setx(-40)
+painter.sety(90)
+painter.pendown()
+
+painter.begin_fill()
+painter.circle(30)
+painter.end_fill()
+
+painter.left(10)
+painter.penup()
+painter.setx(-30)
+painter.sety(110)
+painter.pendown()
+
+painter.color('white', 'white')
+painter.begin_fill()
+painter.circle(15)
+painter.end_fill()
+
+painter.penup()
+painter.setx(-30)
+painter.sety(115)
+painter.pendown()
+
+painter.color('black', 'black')
+painter.begin_fill()
+painter.circle(5)
+painter.end_fill()
+
+
+#Draw right eye
+
+painter.penup()
+painter.setx(40)
+painter.sety(90)
+painter.pendown()
+
+painter.color('black', 'black')
+painter.begin_fill()
+painter.circle(30)
+painter.end_fill()
+
+painter.penup()
+painter.setx(30)
+painter.sety(110)
+painter.pendown()
+
+painter.color('white', 'white')
+painter.begin_fill()
+painter.circle(15)
+painter.end_fill()
+
+painter.penup()
+painter.setx(30)
+painter.sety(115)
+painter.pendown()
+
+painter.color('black', 'black')
+painter.begin_fill()
+painter.circle(5)
+painter.end_fill()
+
+
+#Draw mouth and nose
+
+painter.color('black', 'black')
+painter.penup()
+painter.setx(0)
+painter.sety(50)
+painter.pendown()
+
+painter.begin_fill()
+painter.circle(10)
+painter.end_fill()
+
+painter.right(50)
+painter.circle(20, 100)
+
+painter.penup()
+painter.setx(0)
+painter.sety(50)
+painter.pendown()
+
+painter.circle(20, -100)
+
+turtle.done()
